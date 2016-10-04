919ccaff33d8c1f89fe8e737d7a26f9f73542cb3
shervyna ruan <hsuanwei.ruan@yale.edu>
2016-10-04 01:55:29 +0000

    add nickname and IP localhost

--- server.js
+++ server.js
@@ -4,5 +4,7 @@ var app = express();
 app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
-
-app.listen(process.env.PORT || 4000);
+app.get('/shiny-horse', function(request, response){
+    response.send('I don\'t know why my nickname is shiny-horse.');
+});
+app.listen(process.env.PORT || 4000, process.env.IP || 'localhost');
